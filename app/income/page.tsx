c6a6b0dd5bbdd95f88225a8412412875331e2cdd
Milki Legesse <nitfanono@gmail.com>
2025-09-10 15:11:51 +0300

    The graphs are just mockup for now

--- app/income/page.tsx
+++ app/income/page.tsx
@@ -1,7 +1,84 @@
-import { ArrowUpRight, DollarSign, Plus, Receipt } from "lucide-react";
+import {
+  ArrowUpRight,
+  DollarSign,
+  Eye,
+  Plus,
+  Receipt,
+  SquarePen,
+  Trash,
+} from "lucide-react";
 import ExpenseCard from "../component/expense-card";
+import PieChartComponent from "../component/pie-chart";
+import BarChartComponent, { data } from "../component/bar-chart";
+import DataTable from "../component/table";
+import { ColumnDef } from "@tanstack/react-table";
+import IncomeTable from "../component/income-table";
 
 export default function Income() {
+  const columns: ColumnDef<(typeof data)[0]>[] = [
+    {
+      accessorKey: "Description",
+      header: "Description",
+    },
+    {
+      accessorKey: "Category",
+      header: "Category",
+    },
+    {
+      accessorKey: "Amount",
+      header: "Amount",
+      cell: ({ getValue }) => `$${getValue<number>()}`, // format as currency
+    },
+    {
+      accessorKey: "Date",
+      header: "Date",
+    },
+    {
+      accessorKey: "Status",
+      header: "Status",
+    },
+    {
+      id: "actions",
+      header: "Actions",
+      cell: ({ row }) => (
+        <div className="flex gap-2">
+          <button className="px-2 py-1 bg-white text-black rounded text-sm border border-gray-300 hover:bg-gray-200 cursor-pointer">
+            <Eye className="w-4 h-4"></Eye>
+          </button>
+          <button className="px-3 py-2  bg-white text-black  rounded text-sm border border-gray-300 hover:bg-gray-200 cursor-pointer">
+            <SquarePen className="w-4 h-4" />
+          </button>
+          <button className="px-2 py-1  bg-white text-black  rounded text-sm border border-gray-300 hover:bg-gray-200 cursor-pointer ">
+            <Trash className="w-4 h-4" />
+          </button>
+        </div>
+      ),
+    },
+  ];
+  const data = [
+    // this will be fetched from an api or a graphql
+    {
+      Description: "Lunch with team",
+      Category: "Food",
+      Amount: 45,
+      Date: "2025-09-10",
+      Status: "Approved",
+    },
+    {
+      Description: "Taxi fare",
+      Category: "Transport",
+      Amount: 20,
+      Date: "2025-09-09",
+      Status: "Pending",
+    },
+    {
+      Description: "Taxi fare",
+      Category: "Transport",
+      Amount: 20,
+      Date: "2025-09-09",
+      Status: "Pending",
+    },
+  ];
   return (
     <div className="h-full w-full flex flex-col p-6 pb-2 md:pt-6 gap-4">
       <div className="w-full flex items-center justify-between">
@@ -20,7 +97,7 @@ export default function Income() {
           </button>
         </div>
       </div>
-      <div className="w-full flex flex-col md:flex-row gap-y-2.5 justify-between">
+      <div className="w-full flex flex-col md:flex-row gap-y-2.5 justify-between mb-2.5">
         <ExpenseCard
           title={"Total Income"}
           amount={550.49}
@@ -44,6 +121,39 @@ export default function Income() {
           type={"Pending"}
         />
       </div>
+      <div className=" w-full flex flex-col xl:grid xl:grid-cols-2 gap-x-4">
+        <div className="w-full px-2  border border-gray-300 rounded-lg">
+          <div className="m-3 font-medium">
+            <span className="font-semibold text-lg">
+              Monthly Income Trend <br />
+            </span>
+            <span className="text-gray-400">
+              Your income over the last 6 months
+            </span>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <div className="w-[460px] h-[300px]">
+              <BarChartComponent />
+            </div>
+          </div>
+        </div>
+        <div className="w-full px-2  border border-gray-300 rounded-lg">
+          <div className="m-3 font-medium">
+            <span className="font-semibold text-lg">
+              Income Categories <br />
+            </span>
+            <span className="font-sm text-gray-400">
+              Breakdown of income sources this month
+            </span>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            <div className="w-[300px]">
+              <PieChartComponent />
+            </div>
+          </div>
+        </div>
+      </div>
+      <IncomeTable />
     </div>
   );
 }
